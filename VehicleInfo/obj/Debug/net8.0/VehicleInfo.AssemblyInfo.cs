e9ebc4b31a71323e77cbbd64875d4078481ef32a
AlexJ2006 <alexjakeman@sky.com>
2025-10-20 11:09:40 +0100

    Renaming VehicleData as CarData as that name was when I planned on keeping all of the vehicle data in the same file.

--- VehicleInfo/obj/Debug/net8.0/VehicleInfo.AssemblyInfo.cs
+++ VehicleInfo/obj/Debug/net8.0/VehicleInfo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb49dd4d96d81b43b5eea54c1c0927d237fe9bdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a51f8b63f526144542200db896588b502903087")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
